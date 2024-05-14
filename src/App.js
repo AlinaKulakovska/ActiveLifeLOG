310e207d53e646096cdc60cfd2c1f24374c2ed06
AlinaKulakovska <103748970+AlinaKulakovska@users.noreply.github.com>
2024-05-14 16:36:48 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
 
 import { CiHome, CiLogin } from 'react-icons/ci';
+import { CgAlbum, CgCalendar, CgCalendarDates, CgClose } from 'react-icons/cg';
 import './App.scss';
 import logo from './images/Logo.png'
 import graphs from './images/graphs.png'
 import exercises from './images/exercises.png'
 import personone from './images/person1.png'
 import person from './images/person2.png'
-import { CgClose } from 'react-icons/cg';
+
 import { useState } from 'react';
 function App() {
 
   const [hide, setHide] = useState(true);
   const [logined, setLogined] = useState(false);
-
+  const [hidemenu, setHideMenu] = useState(true);
 
   const validateEmail = () => {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     const email = document.getElementById('email').value;
-    const passwordinput = document.getElementById('password').value;
-    const password = "12345"
-  if (regex.test(email) && passwordinput == password) {
-    setLogined(true);
-    setHide(!hide);
-  } else {
-    setLogined(false);
-    alert("wrong email or password")
-  }
+
+
+    // const passwordinput = document.getElementById('password').value;
+    // const password = "12345"
+    // if (regex.test(email) && passwordinput == password) {
+
+
+    if (regex.test(email)) {
+      setLogined(true);
+      setHide(!hide);
+    } else {
+      setLogined(false);
+      alert("invalid email")
+    }
   }
 
 
   return (
     <div className="App text-white">
-      
+
       <div className={hide ? "hide" : ""}>
         <div className='fixed py-12 px-4 md:py-24 md:px-48 z-30 ' id='login'>
           <button className='text-[#F0FF42] absolute top-5 right-5 text-2xl' onClick={() => setHide(!hide)}><CgClose /></button>
           <h1 className='text-md md:text-lg' >Email</h1>
           <input type='email' id='email' ></input>
           <h1 className='text-md md:text-lg'>Password</h1>
-          <input type='password' className='mb-5'  id='password'></input>
-          <div><div className='button' onClick={() => {validateEmail()}}>Login</div></div>
+          <input type='password' className='mb-5' id='password'></input>
+          <div><div className='button' onClick={() => { validateEmail() }}>Login</div></div>
         </div>
       </div>
 
@@ -48,18 +54,24 @@ function App() {
           <div>
             <img src={logo} className='w-24 md:w-48'></img>
           </div>
-          <div className='flex items-center'>
-            <a href=''>Plan</a>
-            <a href=''>Tracker</a>
+          <div className='flex items-center relative'>
             <div >
-              {logined ?  <CiHome className='text-2xl md:text-4xl lg:text-6xl hover:cursor-pointer' /> : <CiLogin className='text-2xl md:text-4xl lg:text-6xl hover:cursor-pointer' onClick={() => setHide(!hide)}/>}
+              {logined ? <CiHome className='text-2xl md:text-3xl lg:text-5xl hover:cursor-pointer' onClick={() => setHideMenu(!hidemenu)} /> : <CiLogin className='text-2xl md:text-4xl lg:text-6xl hover:cursor-pointer' onClick={() => setHide(!hide)} />}
+            </div>
+
+            <div className={hidemenu ? "hide" : ""}>
+              <div className='absolute right-0 top-[100%] p-2 text-center items-center bg-[#ffffff] text-[#181818]'>
+                <a href='' className='flex items-center '><CgAlbum />Plan</a>
+                <a href='' className='flex items-center'><CgCalendarDates /> Tracker</a>
               </div>
+            </div>
+
           </div>
         </div>
         <div className='h-[40vh] flex flex-col justify-end w-full items-end md:h-[80vh]'>
           <div>
             <p className='text-xl font-bold w-64 mb-4 md:text-4xl md:w-96 drop-shadow-md'>Start <span className='yellow'>Your</span> Fitness Journey with<span className='yellow'> ActiveLifeLOG</span> for Excellent results</p>
-            <a href='' className='button ml-[15%]'>Begin Now!</a>
+            <button className='button ml-[15%]' onClick={() => setHide(!hide)}>Begin Now!</button>
           </div>
         </div>
       </section>
@@ -67,7 +79,7 @@ function App() {
       <section className='flex justify-between px-10 md:px-20 my-24 flex-wrap'>
         <div className='flex justify-center flex-col'>
           <p className='text-lg sm:text-xl  md:text-3xl w-full md:w-64 lg:w-96   mb-4  text-center'><span className='yellow'> Set goals</span> and <span className='yellow'>Track</span> your fitness progress with charts in <span className='yellow'>ActiveLifeLog</span></p>
-          <a href='' className='button'>Track</a>
+          <button className='button' onClick={() => setHide(!hide)}>Track</button>
         </div>
         <img src={graphs} alt='graphs' className='mt-5 md:mt-0 w-auto md:w-[40%] lg:w-[50%]'></img>
       </section>
@@ -76,7 +88,7 @@ function App() {
         <img src={exercises} alt='graphs' className='mt-5 md:mt-0 w-auto md:w-[40%] lg:w-[50%] mb-4 '></img>
         <div className='flex justify-center flex-col'>
           <p className='text-lg  sm:text-xl md:text-3xl w-full md:w-64 lg:w-96  text-center'>Form your<span className='yellow'> very</span> own set of exercises or follow <span className='yellow'>already planned</span> workouts in  <span className='yellow'>ActiveLifeLog</span></p>
-          <a href='' className='button mt-4'>Plan</a>
+          <button className='button mt-4' onClick={() => setHide(!hide)}>Plan</button>
         </div>
       </section>
 
